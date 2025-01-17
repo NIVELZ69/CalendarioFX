6646e56a10242e281397efecb24136cc728bac24
NIVELZ69 <blackwolfshadowkiller@gmail.com>
2025-01-17 08:48:01 +0000

    Ejecución exitosa

--- src/main/java/dad/CalendarFX/App.java
+++ src/main/java/dad/CalendarFX/App.java
@@ -1,13 +1,28 @@
 package dad.CalendarFX;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import dad.CalendarFX.controller.MainController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Cargar el archivo FXML y obtener el controlador
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainView.fxml"));
+        BorderPane root = loader.load(); // Carga la vista principal
+        MainController mainController = loader.getController(); // Obtén el controlador asociado
+
+        // Configurar la escena y el stage
+        primaryStage.setTitle("CalendarFX");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
--- src/main/java/dad/CalendarFX/controller/MainController.java
+++ src/main/java/dad/CalendarFX/controller/MainController.java
@@ -1,13 +1,11 @@
 package dad.CalendarFX.controller;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -31,90 +29,74 @@ public class MainController implements Initializable {
     @FXML
     private GridPane mesesGridPane;
 
-    // Referencias a los componentes personalizados (fx:include)
     @FXML
     private BorderPane eneroMonth;
-
     @FXML
     private BorderPane febreroMonth;
-
     @FXML
     private BorderPane marzoMonth;
-
     @FXML
     private BorderPane abrilMonth;
-
     @FXML
     private BorderPane mayoMonth;
-
     @FXML
     private BorderPane junioMonth;
-
     @FXML
     private BorderPane julioMonth;
-
     @FXML
     private BorderPane agostoMonth;
-
     @FXML
     private BorderPane septiembreMonth;
-
     @FXML
     private BorderPane octubreMonth;
-
     @FXML
     private BorderPane noviembreMonth;
-
     @FXML
     private BorderPane diciembreMonth;
-    
-    public MainController() {
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MonthView.fxml"));
-			loader.setController(this);
-			loader.load();
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
-    
-    private final IntegerProperty year = new SimpleIntegerProperty(2025);
-    
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		// bindings
-//		listaPalabras.setItems(palabrasList);
-//		puntosLabel.textProperty().bind(puntos.asString());
-//		puntos.addListener((Observable, oldValue, newValue) -> actualizarImagen(newValue.intValue()));
-//		listaPuntuaciones.setItems(puntuacionesList);
-    	
-    	try {
-    		for(int i = 0; i < mesesGridPane.getChildren().size(); i++) {
-    			FXMLLoader loader = (FXMLLoader) mesesGridPane.getChildren().get(i).getProperties().get("loader");
-    			MonthController monthController = loader.getController();
-    			
-    			if(monthController != null) {
-    				monthController.yearProperty().bind(yearProperty());
-    				monthController.setMonth(i + 1);
-    			}
-    		}
-    	} catch (Exception e) {
-    		e.printStackTrace();
-    	}
-		
-	}
-    
-    public IntegerProperty yearProperty() {
-    	return year;
-    }
-    
-    public int getYear() {
-    	return year.get();
-    }
-    
-    public void setYear(int year) {
-    	this.year.set(year);
-    }
-    
-}
 
+    private final IntegerProperty year = new SimpleIntegerProperty(2025);
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // Vincula cada mes
+        for (int i = 0; i < mesesGridPane.getChildren().size(); i++) {
+            BorderPane monthPane = (BorderPane) mesesGridPane.getChildren().get(i);
+
+            MonthController monthController = (MonthController) monthPane.getProperties().get("controller");
+            if (monthController != null) {
+                monthController.setMonth(i + 1);
+                monthController.monthProperty().bind(yearProperty());
+            }
+        }
+    }
+
+    public IntegerProperty yearProperty() {
+        return year;
+    }
+
+    public int getYear() {
+        return year.get();
+    }
+
+    public void setYear(int year) {
+        this.year.set(year);
+    }
+
+    @FXML
+    private void onLeftAction() {
+        setYear(getYear() - 1);
+    }
+
+    @FXML
+    private void onRightAction() {
+        setYear(getYear() + 1);
+    }
+
+    public BorderPane getRoot() {
+        return root;
+    }
+
+    public void setRoot(BorderPane root) {
+        this.root = root;
+    }
+}
